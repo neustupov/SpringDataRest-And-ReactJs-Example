c8acb9026ac5ea3ee7fef88f3be6309e79e8d027
Dmitry_Neustupov <neustupov@yandex.ru>
2018-09-02 16:37:29 +0300

    added spring boot, spring data jpa, spring rest, thymeleaf, h2, lombok, spring test

---
+++ src/main/java/ru/neustupov/SpringDataRestAndReactJsExample/SpringDataRestAndReactJsExampleApplication.java
@@ -0,0 +1,12 @@
+package ru.neustupov.SpringDataRestAndReactJsExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestAndReactJsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestAndReactJsExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/neustupov/SpringDataRestAndReactJsExample/SpringDataRestAndReactJsExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.neustupov.SpringDataRestAndReactJsExample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataRestAndReactJsExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
