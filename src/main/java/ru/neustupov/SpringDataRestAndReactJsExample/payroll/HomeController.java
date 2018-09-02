760e18251d12d09d86b2a7278fbbc2ca18df3699
Dmitry_Neustupov <neustupov@yandex.ru>
2018-09-02 18:35:37 +0300

    added custom controller with path for "/"

---
+++ src/main/java/ru/neustupov/SpringDataRestAndReactJsExample/payroll/HomeController.java
@@ -0,0 +1,13 @@
+package ru.neustupov.SpringDataRestAndReactJsExample.payroll;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+}
