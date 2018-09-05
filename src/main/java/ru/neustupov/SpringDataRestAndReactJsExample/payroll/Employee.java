3f8069831a3c6fcecc81a0b098d4b7470a06377a
Dmitry_Neustupov <neustupov@yandex.ru>
2018-09-05 23:04:02 +0300

    added @Version and @JsonIgnore annotation

--- src/main/java/ru/neustupov/SpringDataRestAndReactJsExample/payroll/Employee.java
+++ src/main/java/ru/neustupov/SpringDataRestAndReactJsExample/payroll/Employee.java
@@ -1,21 +1,23 @@
 package ru.neustupov.SpringDataRestAndReactJsExample.payroll;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
 @Data
 @Entity
 public class Employee {
 
-    private @Id
-    @GeneratedValue
-    Long id;
+    private @Id @GeneratedValue Long id;
     private String firstName;
     private String lastName;
     private String description;
+
+    private @Version @JsonIgnore Long version;
 
     private Employee() {
     }
