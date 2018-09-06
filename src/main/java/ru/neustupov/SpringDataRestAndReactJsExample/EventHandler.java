1077c226eb32eb5e9de1ffccb9f9368ca8c201ab
Dmitry_Neustupov <neustupov@yandex.ru>
2018-09-06 09:16:22 +0300

    added event handler class for web sockets

---
+++ src/main/java/ru/neustupov/SpringDataRestAndReactJsExample/EventHandler.java
@@ -0,0 +1,51 @@
+package ru.neustupov.SpringDataRestAndReactJsExample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.HandleAfterCreate;
+import org.springframework.data.rest.core.annotation.HandleAfterDelete;
+import org.springframework.data.rest.core.annotation.HandleAfterSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import ru.neustupov.SpringDataRestAndReactJsExample.payroll.Employee;
+
+import static ru.neustupov.SpringDataRestAndReactJsExample.WebSocketConfiguration.MESSAGE_PREFIX;
+
+@Component
+@RepositoryEventHandler(Employee.class)
+public class EventHandler {
+
+    private final SimpMessagingTemplate websocket;
+
+    private final EntityLinks entityLinks;
+
+    @Autowired
+    public EventHandler(SimpMessagingTemplate websocket, EntityLinks entityLinks) {
+        this.websocket = websocket;
+        this.entityLinks = entityLinks;
+    }
+
+    @HandleAfterCreate
+    public void newEmployee(Employee employee) {
+        this.websocket.convertAndSend(
+                MESSAGE_PREFIX + "/newEmployee", getPath(employee));
+    }
+
+    @HandleAfterDelete
+    public void deleteEmployee(Employee employee) {
+        this.websocket.convertAndSend(
+                MESSAGE_PREFIX + "/deleteEmployee", getPath(employee));
+    }
+
+    @HandleAfterSave
+    public void updateEmployee(Employee employee) {
+        this.websocket.convertAndSend(
+                MESSAGE_PREFIX + "/updateEmployee", getPath(employee));
+    }
+
+    private String getPath(Employee employee) {
+        return this.entityLinks.linkForSingleResource(employee.getClass(),
+                employee.getId()).toUri().getPath();
+    }
+}
